f5bcbac5701b6f0e4e43533a7929d7d4aa3855cb
Yvann <yvann.kreutzenberger@hotmail.fr>
2022-02-02 18:10:40 +0100

    Modifs, ajout commencement logo reseaux

--- Mon Portfolio Web/script.js
+++ Mon Portfolio Web/script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Etudiant", "Developer", "YouTuber", "Designer", "Freelancer"],
+        strings: ["Etudiant", "Developpeur", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Etudiant", "Developer", "YouTuber", "Designer", "Freelancer"],
+        strings: ["Etudiant", "Developpeur", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
